19917ed514ce6faee4f9c9cd5c7224a60b0bc0da
dimasarestuprasmanto <dimas.arestu99@gmail.com>
2020-04-17 09:14:48 +0700

    AKB Tugas 2

--- app/src/main/java/com/dimasarp/akb_tugas2/register.kt
+++ app/src/main/java/com/dimasarp/akb_tugas2/register.kt
@@ -2,11 +2,24 @@ package com.dimasarp.akb_tugas2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ListAdapter
+import android.widget.Spinner
 
 class register : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+
+
+
+
+
     }
 }
